98ff065371f0b5b35175d1ac4f3d793c7f134dfe
Dru Kelly <drukelly@gmail.com>
2019-04-08 19:55:57 -0700

    Initial project set up

---
+++ README.md
@@ -0,0 +1,5 @@
+# Train Scheduler
+A Train scheduler with Firebase DB backend
+
+## CSS Framework
+Tachyons was used as a CSS Framework: https://tachyons.io
---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+// JS
+document.addEventListener("DOMContentLoaded", () => {
+  var config = {
+    apiKey: "AIzaSyAtCII-2h69KfCJlQ1GhguUa9F-vBTF_40",
+    authDomain: "ucbx-coding-bootcamp.firebaseapp.com",
+    databaseURL: "https://ucbx-coding-bootcamp.firebaseio.com",
+    projectId: "ucbx-coding-bootcamp",
+    storageBucket: "ucbx-coding-bootcamp.appspot.com",
+    messagingSenderId: "515667904107"
+  };
+  firebase.initializeApp(config);
+});
